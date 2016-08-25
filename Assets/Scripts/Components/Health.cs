f772cccee1a0f8b70ad959a4c5fa574d552ce07a
jdeal2 <jdeal2@mail.csuchico.edu>
2016-08-25 16:46:17 -0700

    Got done with 1 I believe.

--- Assets/Scripts/Components/Health.cs
+++ Assets/Scripts/Components/Health.cs
@@ -8,14 +8,18 @@ namespace Assets.Scripts.Components
 
 	public class Health: CustomComponentBase {
 		public float CurStuff= 0f;
+        public int startingHealth = 100;
+        public int currentHealth;
 		float pointValue;
 		List<GameObject> collectedLoot;
+        bool isDead = false;
 
 		public void TotalStuff(){
 			GameObject[] tempList = GameObject.FindGameObjectsWithTag ("Loot");
 			float maxStuff = tempList.Length;
 			pointValue = 1f / maxStuff;
 			collectedLoot = new List<GameObject> ();
+            currentHealth = startingHealth;
 
 		}
 
@@ -25,20 +29,36 @@ namespace Assets.Scripts.Components
 				collectedLoot.Add (col.gameObject);
 				col.gameObject.SetActive (false);
 			}
-
 		}
 
-		public void TakeDamage(float damage){
+        void OnCollisionEnter(Collider col)
+        {
+            if (col.gameObject.tag == "Enemy")
+            {
+                currentHealth -= 10;
+                Debug.Log(col.gameObject);
+            }
+        }
+
+        public void TakeDamage(float damage){
 			for (int relicsLost = 0; relicsLost < damage; relicsLost++) {
 				GameObject turnOn = collectedLoot [Random.Range (0, collectedLoot.Count)];
 				collectedLoot.Remove (turnOn);
 				turnOn.SetActive (true);
 				CurStuff -= pointValue;
-			}
+                if (currentHealth <= 0 && !isDead)
+                {
+                    Death();
+                }
+            }
 
 		}
 
 		public void Death(){
+            isDead = true;
+
+            Application.LoadLevel(Application.loadedLevel);
+
 			Debug.Log ("death things");
 		}
 
